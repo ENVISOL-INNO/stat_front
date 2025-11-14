4275c33b92c973fc5150b4678ba1d411860efe4f
JELIAS <j.elias@envisol.fr>
2025-11-14 16:51:13 +0100

    udpate for modelisation and online check

--- components/stats_page.vue
+++ components/stats_page.vue
@@ -90,14 +90,13 @@ const list_champ_visgrid : Ref<Array<Champ>> = ref([
 // --- SWAG API ---
 
 const list_champ_Modelling : Ref<Array<Champ>> = ref([
-  {label: "Forages : Nom | x | y | z | valeur_1... | ... | valeur_n...", name: "drills", type_of_params: "col"     , value: ""},
   {label: "Polygone : x | y | z "                      , name: "polygon"               , type_of_params: "col"     , value: ""},
-  {label: "Code EPSG"                                  , name: "inproj"                , type_of_params: "num"     , value: 0}
-  {label: "Orientation Z"                              , name: "depth_in"              , type_of_params: "string"  , value: "above_sea_level"}
+  {label: "Code EPSG"                                  , name: "inproj"                , type_of_params: "num"     , value: 0},
+  {label: "Orientation Z"                              , name: "depth_in"              , type_of_params: "string"  , value: "above_sea_level"},
   {label: "Grille : Zmin | Zmax"                       , name: "grid_zmin_zmax"        , type_of_params: "num_list", value: [-999, -999]},
   {label: "Grille : dX | dY | dZ"                      , name: "grid_steps"            , type_of_params: "num_list", value: [0, 0, 0]},
-  {label: "Modélisation : Paramètre de forage modélisé", name: "model_parameter"       , type_of_params: "string"  , value: ""}
-  {label: "Modelisation : Valeur seuil"                , name: "model_threshold"       , type_of_params: "num"     , value: 0}
+  {label: "Modélisation : Paramètre de forage modélisé", name: "model_parameter"       , type_of_params: "string"  , value: ""},
+  {label: "Modelisation : Valeur seuil"                , name: "model_threshold"       , type_of_params: "num"     , value: 0},
 ])
        
 </script>

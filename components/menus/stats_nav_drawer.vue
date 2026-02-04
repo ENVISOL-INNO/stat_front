dbce88afaad976657942139f9b2093fbcc1c8e25
JELIAS <j.elias@envisol.fr>
2026-02-04 21:55:19 +0100

    prepa pour mise en prod : retrait de modelisation, clarification des textes, ajout de sub group dans le nav drawer

--- components/menus/stats_nav_drawer.vue
+++ components/menus/stats_nav_drawer.vue
@@ -1,30 +1,50 @@
 <template>
   <v-navigation-drawer>
-    <v-list>
-      <v-list-item>
-        <v-switch v-model="storeNav.show_stat_formulaire" color="primary" label="Statistiques de base"></v-switch>
-      </v-list-item>
-      <v-list-item>
-        <v-switch v-model="storeNav.show_hist" color="primary" label="Histogramme"></v-switch>
-      </v-list-item>
-      <v-list-item>
-        <v-switch v-model="storeNav.show_freq_cum" color="primary" label="Fréquences cumulées"></v-switch>
-      </v-list-item>
-      <v-list-item>
-        <v-switch v-model="storeNav.show_swarmplot" color="primary" label="Swarmplot"></v-switch>
-      </v-list-item>
-      <v-list-item>
-        <v-switch v-model="storeNav.show_boxplot" color="primary" label="Boxplots"></v-switch>
-      </v-list-item>
-      <v-list-item>
-        <v-switch v-model="storeNav.show_modelling" color="primary" label="Modélisation"></v-switch>
-      </v-list-item>
-      <v-list-item>
-        <v-switch v-model="storeNav.show_bm" color="primary" label="Bilan massique"></v-switch>
-      </v-list-item>
-      <v-list-item>
-        <v-switch v-model="storeNav.show_calc_vol" color="primary" label="Calcul de volumes"></v-switch>
-      </v-list-item>    </v-list>
+    <v-list v-model:opened="open">
+      <v-list-group value="Stats">
+        <template v-slot:activator="{ props }">
+          <v-list-item
+            v-bind="props"
+            title="Statistiques"
+            prepend-icon="mdi-chart-bar"
+          ></v-list-item>
+        </template>
+          
+        <v-list-item>
+          <v-switch v-model="storeNav.show_stat_formulaire" color="primary" label="Statistiques de base"></v-switch>
+        </v-list-item>
+        <v-list-item>
+          <v-switch v-model="storeNav.show_hist" color="primary" label="Histogramme"></v-switch>
+        </v-list-item>
+        <v-list-item>
+          <v-switch v-model="storeNav.show_freq_cum" color="primary" label="Fréquences cumulées"></v-switch>
+        </v-list-item>
+        <v-list-item>
+          <v-switch v-model="storeNav.show_swarmplot" color="primary" label="Swarmplot"></v-switch>
+        </v-list-item>
+        <v-list-item>
+          <v-switch v-model="storeNav.show_boxplot" color="primary" label="Boxplots"></v-switch>
+        </v-list-item>  
+      </v-list-group>  
+      <v-list-group  value="Modélisation">
+        <template v-slot:activator="{ props }">
+          <v-list-item
+            v-bind="props"
+            title="Modélisation"
+            prepend-icon="mdi-earth"
+          ></v-list-item>
+        </template>
+        <v-list-item>
+          <v-switch v-model="storeNav.show_modelling" color="primary" label="Modélisation"></v-switch>
+        </v-list-item>
+        <v-list-item>
+          <v-switch v-model="storeNav.show_bm" color="primary" label="Bilan massique"></v-switch>
+        </v-list-item>
+        <v-list-item>
+          <v-switch v-model="storeNav.show_calc_vol" color="primary" label="Calcul de volumes"></v-switch>
+        </v-list-item>
+      </v-list-group>  
+    </v-list>
     <!-- <v-switch v-model="mode_spectro" color="primary" label="Spectro"></v-switch> -->
   </v-navigation-drawer>
 </template>
@@ -33,8 +53,9 @@
 
 import { useMyNavStore } from '#build/imports';
 const storeNav = useMyNavStore();
-
-
+const stats_open = ref(true);
+const modelisation_open = ref(false);
+const open = ref(['Users'])
 // let show_stat_formulaire = ref(true)
 // let show_swarmplot = ref(false)
 // let show_boxplot = ref(false)
--- components/stats_page.vue
+++ components/stats_page.vue
@@ -1,10 +1,34 @@
 <template>
   <MenusStatsNavDrawer />
+    <h1>Vous voulez :</h1>
 
-  <div>
+    <v-expansion-panels multiple>
+      <v-expansion-panel>
+        <v-expansion-panel-title><h2>Faire des stats</h2></v-expansion-panel-title>
+        <v-expansion-panel-text>
+          Le fichier attendu contient :<br>
+          - des colonne avec les noms, concentrations mesurées, lithologies (etc.) des échantillons<br>
+          - des lignes avec les échantillons<br><br>
+          
+          Il sera traité comme suit :<br>
+          - les "nd", "<" sont transformés en 0<br>
+          - les "na" et "-" sont considérés comme non analysé<br>
+          Tout autre texte ("non analysé" par exemple) n'est pas pris en compte et faussera les résultats !
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+      <v-expansion-panel>
+        <v-expansion-panel-title><h2>Exploiter un modèle géostats</h2></v-expansion-panel-title>
+        <v-expansion-panel-text>
+          Le fichier attendu doit contenir :<br>
+          - les colonnes x, y, et z (il faut que ces noms soient respectés)<br>
+          - des colonnes avec les concentrations modélisées
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  <br>
+  <!-- <div>
     <v-card>
       <v-card-text>
-        <!-- Maybe this should be hidable? -->
         <h1>Bienvenue sur l'interface pour l'analyse exploratoire de données !</h1>
         Le fichier donné sera traité comme suit :<br>
         - "nd", "<" sont transformés en 0<br>
@@ -12,7 +36,7 @@
           Tout autre texte ("non analysé" par exemple) n'est pas pris en compte et faussera les résultats !<br>
       </v-card-text>
     </v-card>
-  </div>
+  </div> -->
 
   <!-- <div>
         <VFileInput v-model="files" label="Selectionner fichier"></VFileInput>
@@ -21,12 +45,9 @@
   <div>
     <File_import> </File_import>
   </div>
-  <div v-if="storeNav.show_stat_formulaire">
+  <div padding="100px" v-if="storeNav.show_stat_formulaire">
     <Formulaire_stats_de_base v-bind:data="store.data_csv"></Formulaire_stats_de_base>
   </div>
-  <!-- <div v-if="storeNav.show_stat_formulaire">
-    <FormulaireGeneralise name="swarmplot" endpoint_name="/EDASwarmPlot" :champs=list_champ_swarmplot :data="store.data_csv"></FormulaireGeneralise>
-  </div> -->
 
   <div v-if="storeNav.show_swarmplot">
     <FormulaireGeneralise name="Swarmplot" backend="" endpoint_name="/EDASwarmPlot" :champs=list_champ_swarmplot store_name="MyData_and_resultsStore"></FormulaireGeneralise>
@@ -40,31 +61,20 @@
   <div v-if="storeNav.show_hist">
     <FormulaireGeneralise name="Histogramme" backend="" endpoint_name="/EDAHistogram" :champs=list_champ_histo store_name="MyData_and_resultsStore"></FormulaireGeneralise>
   </div>
-  <!-- <div v-if="storeNav.show_freq_cum && mode == 'spectro'">
-    <FormulaireGeneralise name="visualisation grid" endpoint_name="/VisGrid" :champs=list_champ_visgrid store_name="MyData_and_resultsStore"></FormulaireGeneralise>
-  </div>
-   <-- SWAG API -->
+
   <div v-if="storeNav.show_modelling && mode == 'spectro' && mode == 'spectro'">
     <FormulaireGeneralise name="Modélisation" :backend=backeng_url_swag endpoint_name="/modelisation_auto" :champs=list_champ_modelling store_name="MyData_and_resultsStore"></FormulaireGeneralise>
   </div>
-  <div v-if="storeNav.show_bm && mode == 'spectro' && mode == 'spectro'">
+  <div v-if="storeNav.show_bm">
     <FormulaireGeneralise name="Bilan massique" :backend=backeng_url_swag endpoint_name="/masse_volume_analysis_table" :champs=list_champ_bm store_name="MyData_and_resultsStore"></FormulaireGeneralise>
   </div>
-  <div v-if="storeNav.show_calc_vol && mode == 'spectro' && mode == 'spectro'">
+  <div v-if="storeNav.show_calc_vol">
     <FormulaireGeneralise name="Calculs de volume" :backend=backeng_url_swag endpoint_name="/access_volume_analysis_table" :champs=list_champ_calc_vol store_name="MyData_and_resultsStore"></FormulaireGeneralise>
   </div>
-  <div>
-    <!-- <DownloadBlobBtn :data="brr" é"filename="ff" /> -->
-
-  </div>
-
 
 </template>
 
 <script setup lang="ts">
-const strin_arr : string = '[{"z":3.5, "value":0.009058,"x":1.3373074042,"y":48.0687111932},{"z":4.5,"value":0.0,"x":1.3373074042,"y":48.0687111932},{"z":5.5,"value":0.0,"x":1.3373074042,"y":48.0687111932},{"z":6.5,"value":0.0,"x":1.3373074042,"y":48.0687111932},{"z":0.5,"value":0.206616,"x":1.337340469,"y":48.0685136444},{"z":1.5,"value":0.01,"x":1.337340469,"y":48.0685136444}]'
-// transform strin_arr into an array
-const brr : Array<string> = JSON.parse(strin_arr);
 
 import { useMyData_and_resultsStore } from '~/stores/data_and_results';
 import { useMyNavStore } from '~/stores/nav';
@@ -105,20 +115,28 @@ const list_champ_boxplot : Ref<Array<Champ>> = ref([
 
 // --- SWAG API ---
 
+
 const list_champ_modelling : Ref<Array<Champ>> = ref([
   // {label: "Polygone : x | y | z "            , name: "polygon"               , type_of_params: "string"  , value: ""},
-  {label: "Modélisation : Paramètre modélisé", name: "model_parameter"       , type_of_params: "col"     , value: ""},
-  {label: "Modelisation : Valeur seuil"      , name: "model_threshold"       , type_of_params: "num"     , value: 0},
-  {label: "Grille : dim. X dim. Y dim. Z"    , name: "grid_steps"            , type_of_params: "num_list", value: "5 5 1"},
-  {label: "Grille : Zmin, Zmax"              , name: "grid_zmin_zmax"        , type_of_params: "num_list", value: "0 1"},
-  {label: "Orientation Z"                    , name: "depth_in"              , type_of_params: "string"  , value: "relative"},
-  {label: "Code EPSG"                        , name: "inproj"                , type_of_params: "num"     , value: 2154},
-  {label: "colonne avec nom ech."            , name: "drillhole_col_name"    , type_of_params: "col"  , value: ""},
+  {label: "Paramètre à modélisé"             , name: "model_parameter"       , type_of_params: "col"     , value: ""},
+  {label: "Colonne avec nom ech."            , name: "drillhole_col_name"    , type_of_params: "col"  , value: ""},
+  {label: "Grille de modélisation : taille de la cellule élémentaire en x en y en z séparées par un espace"    , name: "grid_steps"            , type_of_params: "num_list", value: "5 5 1"},
 ])
+
+// const list_champ_modelling : Ref<Array<Champ>> = ref([
+//   // {label: "Polygone : x | y | z "            , name: "polygon"               , type_of_params: "string"  , value: ""},
+//   {label: "Paramètre à modélisé"             , name: "model_parameter"       , type_of_params: "col"     , value: ""},
+//   {label: "Colonne avec nom ech."            , name: "drillhole_col_name"    , type_of_params: "col"  , value: ""},
+//   {label: "Modelisation : Valeur seuil"      , name: "model_threshold"       , type_of_params: "num"     , value: 0},
+//   {label: "Grille de modélisation : taille de la cellule élémentaire en x en y en z séparées par un espace"    , name: "grid_steps"            , type_of_params: "num_list", value: "5 5 1"},
+//   {label: "Grille : Zmin, Zmax"              , name: "grid_zmin_zmax"        , type_of_params: "num_list", value: "0 1"},
+//   {label: "Z est exprimé en :"               , name: "depth_in"              , type_of_params: "string"  , value: "relative"},
+//   {label: "Système de géoréférencement : code EPSG"                          , name: "inproj"                , type_of_params: "num"     , value: 2154},
+// ])
 
 const list_champ_bm : Ref<Array<Champ>> = ref([
   // {label: "Fichier avec grille", name: "grid_df", type_of_params: "file", value: ""},
-  {label: "Grille : dim. X dim. Y dim. Z", name: "grid_steps", type_of_params: "num_list", value: "5 5 1"},
+  {label: "taille de la cellule élémentaire en x en y en z séparées par un espace", name: "grid_steps", type_of_params: "num_list", value: "5 5 1"},
   {label: "Paramètre modélisé", name: "sim_str_identifier", type_of_params: "col", value: ""},
   {label: "Densité sol", name: "soil_density", type_of_params: "num", value: 1.8},
   {label: "Concentrations séparées par un espace (ex. 0 200 1000) (si vide des valeurs automatiques seront déterminé)", name: "concentration_threshold_list_table", type_of_params: "num_list", value: ""},
@@ -127,11 +145,11 @@ const list_champ_bm : Ref<Array<Champ>> = ref([
 
 const list_champ_calc_vol : Ref<Array<Champ>> = ref([
   // {label: "Fichier avec grille", name: "grid_df", type_of_params: "file", value: ""},
-  {label: "Grille : dim. X dim. Y dim. Z", name: "grid_steps", type_of_params: "num_list", value: "5 5 1"},
+  {label: "taille de la cellule élémentaire en x en y en z séparées par un espace", name: "grid_steps", type_of_params: "num_list", value: "5 5 1"},
   {label: "Paramètre modélisé", name: "pollutants_names", type_of_params: "col_list", value: []},
   {label: "Valeur min.", name: "pollutants_min_values", type_of_params: "num_list", value: "0"},
   {label: "Valeur max.", name: "pollutants_max_values", type_of_params: "num_list", value: "999999999"},
-  {label: "Orientation Z", name: "mode_z", type_of_params: "string", value: "relativ"},
+  {label: "Z est exprimé en :", name: "mode_z", type_of_params: "string", value: "relativ"},
 ])
 
 </script>
